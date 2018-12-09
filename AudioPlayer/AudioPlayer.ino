34cffd5d5b0647a46b9fe932cc3318d1ee988427
kwiskel <kwiskel@gmail.com>
2018-12-09 14:13:04 -0700

    Final Version

--- AudioPlayer/AudioPlayer.ino
+++ AudioPlayer/AudioPlayer.ino
@@ -9,29 +9,44 @@
 
 
 #include <SD.h> // library to read/write to SD cards
-#include<TMRpcm.h>  // Audio library allows asynchronous playback , uses Timer and PWM output from digital WAV signal
+#include<TMRpcm.h>  // Audio library allows asynchronous playback , uses Timer
 
 
 const int SDpin = 10; // SD Card Reader Chip Select Pin  -- default of 10 
 TMRpcm audio; //new instance of TMRpcm
-//String songFiles[];
-int songCount;
-long startTime = 0;
-bool playStatus = false;
-int currentVolume = 5;
+
+
+int songCount; // the number of WAV files read from the SD card
+long startTime = 0; // time since a song started playing -- > to compute remaining playback time
+bool playStatus = false; // if a song is playing or not
+int currentVolume = 5; // current Volume of the audio (between 0 and 6)
 int previous[5]; // previous 5 played songs --> lowest index most recent played
 
 
 
 
 
-int32_t littleEndian(File file,int index, int n){
+int32_t littleEndian(File file,int index, int n) {
+
+  /* Raeds the bytes in Little Endian
+
+    Arguments:
+        File file: The file to read from
+        int index: The index of the first byte to read
+        int n: The number of bytes to read starting at index
+        
+
+    Return:
+        int32_t value: The integer value of the bytes
+    
+    */
+  
   int32_t value = 0;
-  file.seek(index);
-  for (int i = 0; i < n; i++){
-    //Serial.println(int32_t(bytes[i]),BIN);
-    value += (int32_t(file.read()) << (i*8));
-    //Serial.println(value,BIN);
+  
+  file.seek(index); // begins at index byte
+  
+  for (int i = 0; i < n; i++) {
+    value += (int32_t(file.read()) << (i*8)); // first byte read is LSB byte
     
   }
   
@@ -39,23 +54,34 @@ int32_t littleEndian(File file,int index, int n){
   
 }
 
-void readHeader(File file){
+void readHeader(File file) {
+
+  /* Reads the header of a wav file and prints out the results
+
+    Arguments:
+        File file: The file to read the header from
+
+    Return:
+        
+    
+    */
+    
   String fields[] = {"ChunkID","ChunkSize", "Format", "SubChunk1ID", "Subchunk1Size", "AudioFormat", "NumChannels", "SampleRate",
   "ByteRate","BlockAlign","BitsPerSamples","SubChunk2ID","Subchunk2size"}; //44 bytes waved file RIFF header
-  int fieldSize[] = {4,4,4,4,4,2,2,4,4,2,2,4,4};
+  int fieldSize[] = {4,4,4,4,4,2,2,4,4,2,2,4,4}; // size of each field in the header
   char endian[] = {'b','l','b','b','l','l','l','l','l','l','l','b','l'}; // RIFF byte order scheme
   int index = 0;
-  for (int i = 0; i < sizeof(fieldSize)/sizeof(fieldSize[0]);i++){
+  
+  for (int i = 0; i < sizeof(fieldSize)/sizeof(fieldSize[0]);i++) { 
     Serial.print(fields[i] + ": ");
-    if(endian[i] == 'b'){ // big endian format 
+    if(endian[i] == 'b') { // big endian format 
       file.seek(index);
-      for (int j = 0; j < fieldSize[i]; j++){
-        //Serial.print(j);
-        Serial.print(char(file.read()));
+      for (int j = 0; j < fieldSize[i]; j++) {
+        Serial.print(char(file.read())); // reads the next j bytes
       }
       index += fieldSize[i];
       Serial.println();
-    }else if(endian[i] == 'l'){ // convert to big endian format and print
+    } else if(endian[i] == 'l') { // little endian format
       Serial.println(littleEndian(file,index,fieldSize[i]));
       index += fieldSize[i];
     }
@@ -64,6 +90,16 @@ void readHeader(File file){
 
 
 void setup() {
+
+  /* Setups up the Audio Library, and checks if SD card is connected
+
+    Arguments:
+        
+
+    Return:
+       
+    
+    */
 
   // Arduino communicates with SD card reader using SPI (SPI library needed) -- Arduino as master, Serial Peripheral Interface
   // Synchronous communcation, 
@@ -76,15 +112,6 @@ void setup() {
   Serial.println("\nFiles found on the card (name, date and size in bytes)");
   Serial.print("Begin SD: ");
   Serial.println(SD.begin(SDpin));
-  /*File file = SD.open("/"); // instance of file class returned by SD.open(), checks for WAV files on SD card starting in root folder and checking all folders
-  songCount = searchWav(file,"/",0,0, {});  // counts number of wav files
-  Serial.print("Total number of songs: ");
-  Serial.println(songCount); // prints out number of songs found
-  String songFiles[songCount];
-  file = SD.open("/"); // opens file in root directory
-  searchWav(file,"/",0,1,songFiles); // creates an array of song fileName (function edits array (don't need to return array)) 
-  // -- calls same function twice so as to avoid dynamic array creation
-  Serial.println();*/
   // connect to other Arduino -- (using code from Assignment#2)
   // "server" arduino is Arduino with Audio, "client" Arduino is arduino with LCD
   
@@ -95,10 +122,16 @@ void setup() {
 }
 
 void loop() {
-  // get data from serial3?
-  // decide what to do with audio (play/pause)
-  // return time played (or print in serial)
-  // use loop as main() right now
+
+  /* Reads WAV files from SD card and loops continuously checking for playback input from other Arduino
+
+    Arguments:
+
+    Return:
+    
+    */
+    
+
   File file = SD.open("/"); // instance of file class returned by SD.open(), checks for WAV files on SD card starting in root folder and checking all folders
   songCount = searchWav(file,"/",0,0, {});  // counts number of wav files
   Serial.print("Total number of songs: ");
@@ -110,51 +143,36 @@ void loop() {
   file.close();
   Serial.println("done setup");
   
- /* Serial.println();
-  File file1 = SD.open("SONG0.WAV");
-  Serial.println(file1.name());
-  readHeader(file1);  // prints out header info
-  printLength(songLength(file1)); // prints out length of song in minutes/seconds
-  Serial.flush();
-  file1.close();
-  Serial.flush();
-  Serial.println(getName(songFiles[0]));
-  while(true){
-    
-  }*/
-  
-  
-  if(serverSetup()){
-    //sendInitialInfo(songFiles); 
-   // Serial.println("Done sendin");
+  if(serverSetup()) { // sets up server and client
+    sendInitialInfo(songFiles);  // sends song info (name + artist)
+    Serial.println("Done sendin");
   }
-  //Serial.println("loop");
-  while(true){
-   // Serial.println("loop");
-    if (Serial3.available()){
+  
+  while(true) { // loops forever
+    if (Serial3.available()) { // if byte is available from other Arduino
       char byteR = Serial3.read();
       Serial.println(byteR);
-      if(byteR == 'p' && audio.isPlaying()){
-        playPause();
+      if(byteR == 'p' && audio.isPlaying()) {
+        playPause(); // plays or pauses the audio
         if(playStatus){
           Serial.println("playing");
         }else{
           Serial.println("paused");
         }
-      }else if(byteR == 's'){ //play new song --> next byte is index of song
+      } else if(byteR == 's') { //play new song --> next byte is index of song
         while(!Serial3.available()){
           
         }
         char song = Serial3.read();
         int index = int(song); // convert char to int 
-        //Serial.println(index);
-        playSong(songFiles[index]); // play new song (also shift previous);
-        shiftPrevious(int(song));
-      }else if(byteR == 'u'){ // volume up
+
+        playSong(songFiles[index]); // play new song;
+        shiftPrevious(int(song)); // shifts previous (creates array of previous played songs)
+      } else if(byteR == 'u') { // volume up
         currentVolume = changeVolume(currentVolume,1);
         Serial.print("Current volume: ");
         Serial.println(currentVolume);
-      }else if(byteR == 'd'){ // volume down
+      } else if(byteR == 'd') { // volume down
         currentVolume = changeVolume(currentVolume,0);
         Serial.print("Current volume: ");
         Serial.println(currentVolume);
@@ -163,25 +181,7 @@ void loop() {
   }
 }
 
-bool clientSetup(){ // LCD Arduino Setup
-  /*  Client sends 'a' --> ask for connection
-   *  Server sends 's' --> server acknowlegment
-   *  Client sends 'c' --> client acknowlegment
-   *  Then connected --> client can send starting info
-   */
-   Serial3.write('a'); // conection request
-   while(true){ // keep looking for 's'
-    if(Serial3.available()){
-      char byteR = Serial3.read();
-      if(byteR == 's'){ // server acknowlegment
-        Serial.println("Connected");
-        Serial3.write('c'); // acknowlegment
-        return true;
-      }
-    }
-   }
-   return false;
-}
+
 
 void sendInitialInfo(String files[]){ // send any information to other Arduino (song information)
   //Serial.flush();
@@ -236,25 +236,35 @@ void sendInitialInfo(String files[]){ // send any information to other Arduino (
 }
 
 
-bool serverSetup(){ // Audio Arduino Setup
-  /*  Client sends 'a' --> ask for connection
+bool serverSetup(){ 
+
+  /* Setups up the Arduino as a Server (with other Arduino acting as client)
+
+    Arguments:
+
+    Return:
+        bool: True of connection successful
+    
+   
+   *  Client sends 'a' --> ask for connection
    *  Server sends 's' --> server acknowlegment
    *  Client sends 'c' --> client acknowlegment
    *  Then connected --> client can send starting info
    */
+   
   Serial.println("Waiting to connect");
   while(true){ // keep looking for 'a'
     if(Serial3.available()){
       char byteR = Serial3.read();
-      if(byteR == 'a'){ //received request from client
+      if(byteR == 'a') { //received request from client
         Serial.println("Connection Request");
         Serial3.write('s'); //server responds
         Serial.println("Server acknowlegment");
-        while(true){
+        while(true) {
           // wait for acknowlegment
-          if(Serial3.available()){
+          if(Serial3.available()) {
             byteR = Serial3.read();
-            if(byteR == 'c'){ // client acknowlegment
+            if(byteR == 'c') { // client acknowlegment
               Serial.println("Connected");
               return true;
             }
@@ -267,20 +277,29 @@ bool serverSetup(){ // Audio Arduino Setup
 }
 
 
-void sendSerial3(char bytes[],int size){
-  //int aSize = sizeof(bytes)/sizeof(bytes[0]); 
-  for (int i = 0;  i < size; i ++){
+void sendSerial3(char bytes[],int size) {
+
+  /* Sends bytes to Serial3
+
+    Arguments:
+        char bytes[]: Array of bytes to send
+        int size: The number of bytes to send
+
+    Return:
+    
+    */
+    
+  for (int i = 0;  i < size; i ++) {
     Serial3.write(bytes[i]);
     delay(10);
-    //Serial.print(bytes[i]);
   }
-  //Serial.println();
+  Serial3.flush();
 }
 
 
+//implement next functions on client code
 
-// following 2 functions can be implemented in LCD code (arduino)
-void shuffleSong(){
+void shuffleSong() {
   //pick random song next (based on previous songs played)
   int nextRandom = 0;
   while(true){
@@ -301,7 +320,7 @@ void shuffleSong(){
   }
 }
 
-void shiftPrevious(int last){ //shifts data in previous songs (when starting playing song);
+void shiftPrevious(int last) { //shifts data in previous songs (when starting playing song);
   for (int i = 4; i > 1; i++){
     previous[i] = previous[i-1]; // shift and loses 6th recently played
   }
@@ -310,31 +329,51 @@ void shiftPrevious(int last){ //shifts data in previous songs (when starting pla
 }
 
 
-int songLength(File file){ // returns the length of the song in seconds
+int songLength(File file) { 
+
+  /* Computes the playback length of the WAV file in seconds
+
+    Arguments:
+       File file: The WAV file to compute playback length of
+
+    Return:
+        int seconds: The playback length in seconds
+    
+    */
+    
   int seconds = 0;
+  
   // read number of data bytes from file header (SubChunk2Size) @ byte 40 following RIFF header formatting
-  // also need to read byteRate (number of bytes per second)
 
   // call littleEndian function to calculate byteSize of data chunk
   int32_t byteSize = littleEndian(file,40,4);
   //Serial.println(byteSize);
   
   
-  // byteRate
+  // read byteRate from file header
   int32_t byteRate = littleEndian(file,28,4);
-  //Serial.println(byteRate);
-  seconds = byteSize/byteRate;
+
+  seconds = byteSize/byteRate; // calculates number of seconds
   return seconds;
-  
   
 }
 
 
 
-void printLength(int seconds){
-  // prints out length of song in minutes and seconds
+void printLength(int seconds) {
+  /* Prints the length of the song in minutes and seconds
+
+    Arguments:
+        int seconds: The length of the song in seconds
+
+    Return:
+       
+    
+    */
+
+    
   int minutes = 0;
-  while(seconds >= 60){
+  while(seconds >= 60) {
     minutes+=1;
     seconds -=60;
   }
@@ -346,39 +385,72 @@ void printLength(int seconds){
 }
 
 
-String getName(String f){
-  // returns name of song 
+String getName(String f) {
+  
+  /* Gets the name of the song
+
+    Arguments:
+        String f: The name of the file (location on SD card)
+
+    Return:
+        String: The name of the song
+    
+    */
+
+    
   char fName[f.length()];
   f.toCharArray(fName,f.length()+1);
-  //Serial.print("Fname: ");
-  //Serial.println(fName);
+
+ 
   char songName[32];
-  audio.listInfo(fName,songName,0);
+  audio.listInfo(fName,songName,0); // uses library to get song name
   return (String(songName));
+  
 }
 
-String getArtist(String f){
-  // returns artist of song
+String getArtist(String f) {
+  
+  /* Gets the artist name of the song
+
+    Arguments:
+       String f: The name of the file (location on SD card)
+
+    Return:
+        String: The name of the Artist
+    
+    */
+
+
   char fName[f.length()];
   f.toCharArray(fName,f.length()+1);
   char artistName[32];
   audio.listInfo(fName,artistName,1);
-  //Serial.println(artistName);
   return (String(artistName));
+  
 }
 
 
 
 int searchWav(File dir,String location, int count, int save, String arr[]){ 
-  // dir = directory to check for wav files
-  // location = file location from root directory
-  // count = the number of wav files found
-  // save = 1 --> save the file names to arr[], 0 --> simply count number of wav files (so as to use static array when allocating)
-  // arr[] = the array of fileNames
+  
+  /* Searches the SD card for WAV files (first count then save names) --> recursive algorithm
+  
+    Arguments:
+        File dir: The current opened directory
+        String location: The location of the directory from the root folder on the SD card
+        int count: The count of WAV files found
+        int save: Whether or not to save the file names to the array (1 to save 0 to simply count)
+        String arr[]: Array to save file names to
+
+    Return:
+        int count: The count of the number of WAV files found
+    
+    */
+    
 
   // prints only .wav files that are greater than 4 bytes --> audio files to play
   
-  while(true){ // while still a file to read
+  while(true) { // while still a file to read
     File entry = dir.openNextFile();
     if(!entry){
       return count; // returns count of wav files found so far
@@ -386,15 +458,14 @@ int searchWav(File dir,String location, int count, int save, String arr[]){
       break;
     }
     if(entry.isDirectory()){
-      //Serial.println(entry.name());
+      //if new directory then enter that directory (calls itself)
       count = searchWav(entry,location + entry.name() + "/", count, save, arr);
     }
     String fName = String(entry.name());   
     if(entry.size() > 4096 && fName.substring(fName.length()-3,fName.length()) == "WAV"){  // greater than 4 bytes and wav format
       if(save == 1){ // if saving songs to array
         arr[count] = location + entry.name();
-        //Serial.println(arr[count]);
-        //Serial.println(entry.size());
+ 
         delay(10);
       }
      
@@ -405,22 +476,28 @@ int searchWav(File dir,String location, int count, int save, String arr[]){
   
 }
 
-void playSong(String f){ // plays new song with file location as input
+void playSong(String f){ 
+  /* Plays a new song
+
+    Arguments:
+        String f: The file name to play (location from root folder on SD card)
+
+    Return:
+
+    
+    */
+
+    
   char fName[f.length()];
   f.toCharArray(fName,f.length()+1);
-  if(audio.isPlaying()){ // check if playing another song and stop playback
-    //Serial.println("audioplaying");
-    
+  if(audio.isPlaying()) { // check if playing another song and stop playback    
     audio.stopPlayback();
   } 
- // Serial.println(fName);
+  
+
    // play new file
-   audio.setVolume(5);
+   audio.setVolume(currentVolume);
    audio.play(fName);
-  //Serial.print("Playing Song: ");
-  //Serial.println(getName(f));
-  //Serial.print("Artist: ");
-  //Serial.println(getArtist(f));
   f.toCharArray(fName,f.length()+1);
   File file = SD.open(fName);
   //Serial.println(file.name());
@@ -429,8 +506,8 @@ void playSong(String f){ // plays new song with file location as input
   Serial.flush();
   file.close();
   playStatus = true;
-  delay(100);
 }
+
 
 void playPause(){ // pauses or plays song
   if(playStatus){
@@ -441,11 +518,11 @@ void playPause(){ // pauses or plays song
   audio.pause();
 }
 
-void stopSong(){
+void stopSong(){ // stops song 
   audio.stopPlayback();
 }
 
-int changeVolume(int currentVolume, int change){
+int changeVolume(int currentVolume, int change){ // changes volume of song
   
   if(change == 1){ //up volume by 1
      if(currentVolume == 6){
